0434baf5612282165687156f13296d7265cedfcf
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-20 12:23:25 -0500

    Update HTML_Shortner.py

--- Shortener/HTML_Shortner.py
+++ Shortener/HTML_Shortner.py
@@ -1,9 +1,12 @@
+# Prompt for url
 url_input = input("Enter a url to shorten: ")
+# If it doesnt with https:// or http:// then add it
 if url_input.startswith("https://" or "http://"):
     pass
 else:
     url_input = "https://" + url_input
 user_input = input("Enter a word (this word will be you shortner name): ")
+# Inserts the code
 html = f"<a href=\"{url_input}\"><h1>{user_input}</h1></a>"
 html_file = open("Shortner.html", "w")
 html_file.write(html)
