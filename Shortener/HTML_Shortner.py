bfed909781fb436498c110f0c1adad36af42b9a9
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-21 15:39:24 -0500

    Update HTML_Shortner.py

--- Shortener/HTML_Shortner.py
+++ Shortener/HTML_Shortner.py
@@ -1,13 +1,17 @@
-# Prompt for url
-url_input = input("Enter a url to shorten: ")
-# If it doesnt with https:// or http:// then add it
-if url_input.startswith("https://" or "http://"):
-    pass
-else:
-    url_input = "https://" + url_input
-user_input = input("Enter a word (this word will be you shortner name): ")
-# Inserts the code
-html = f"<a href=\"{url_input}\"><h1>{user_input}</h1></a>"
-html_file = open("Shortner.html", "w")
-html_file.write(html)
-html_file.close()
+try:
+    url_input = input("Enter a url to shorten: ")
+    if url_input.startswith("https://" or "http://"):
+        pass
+    else:
+        url_input = "https://" + url_input
+    user_input = input("Enter a word (this word will be you shortner name): ")
+    Size = int(input("Pick the size of your url\nOptions are 1-6. 1 is the largest and 6 is the smallest: "))
+    if Size in range(1, 7):
+        html = f"<a href=\"{url_input}\"><h{Size}>{user_input}</h{Size}></a>"
+        html_file = open("Shortener.html", "w")
+        html_file.write(html)
+        html_file.close()
+    else:
+        print("Invalid option")
+except ValueError:
+    print("You have entered a string not a number")
